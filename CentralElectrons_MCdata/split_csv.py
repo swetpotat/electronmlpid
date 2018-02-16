7678e5f54dbca60fc9534699e610b672fcdca950
Priyanka Cheema <pche3675@sydui1.syd.coepp.org.au>
2018-02-16 14:49:36 +1100

    modified for 500000 events

--- CentralElectrons_MCdata/split_csv.py
+++ CentralElectrons_MCdata/split_csv.py
@@ -1,14 +1,14 @@
 import csv
 
-filename_sig = "MC_SigElectrons_2000000ev.csv"
-filename_bkg = "MC_BkgElectrons_2000000ev.csv"
+filename_sig = "MC_SigElectrons_500000ev.csv"
+filename_bkg = "MC_BkgElectrons_500000ev.csv"
 
 out_sig = csv.writer(open(filename_sig, 'w+'), delimiter = ',')
 out_bkg = csv.writer(open(filename_bkg, 'w+'), delimiter = ',')
 
 counter = 0
 
-with open('data/MC_SigBkgElectrons_2000000ev.csv') as f:
+with open('data/MC_SigBkgElectrons_500000ev.csv') as f:
 	csvfile = csv.reader(f, delimiter = ',')
 	for row in csvfile:
 		if counter != 0:
