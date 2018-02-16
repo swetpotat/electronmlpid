72b8211034e65a70869d245d66019458bfc5ac4c
Priyanka Cheema <pche3675@sydui1.syd.coepp.org.au>
2018-02-16 14:07:48 +1100

    fixed path

--- CentralElectrons_MCdata/csv_to_root.py
+++ CentralElectrons_MCdata/csv_to_root.py
@@ -1,12 +1,12 @@
 from ROOT import *
 
 # Creating new root file
-hfile = TFile('MC_SigBkgElectrons_2000000ev.root','RECREATE')
+hfile = TFile('MC_BkgElectrons_2000000ev.root','RECREATE')
 
 print("Filling up ntuples...")
 
 tree = TTree('data','data')
-tree.ReadFile('MC_SigBkgElectrons_2000000ev.csv','',',')
+tree.ReadFile('data/MC_BkgElectrons_2000000ev.csv','',',')
 
 print("Saving ntuple to file and closing file.") 
 
