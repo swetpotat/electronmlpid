f0a427d30de4481031c0170830c109d76852851d
Priyanka Cheema <pche3675@sydui1.syd.coepp.org.au>
2018-03-02 04:47:19 +0000

    fixed to draw ROC properly

--- CentralElectrons_MCdata/ROC.py
+++ CentralElectrons_MCdata/ROC.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-from ROOT import TFile, TCanvas, TH1F
+from ROOT import TFile, TCanvas, TGraph, TH1F
 from ROOT import gDirectory, std
 import sys, getopt
 import math
@@ -14,55 +14,87 @@ def main():
 	gROOT.SetBatch(True)
 
 	# Open root files
-	rfile = TFile('/home/pche3675/summer2018/electronmlpid/CentralElectrons_MCdata/data/MC_SigElectrons_2000000ev.root')
+	sig_file = TFile('/home/pche3675/summer2018/electronmlpid/CentralElectrons_MCdata/data/MC_SigElectrons_2000000ev.root')
+	bkg_file = TFile('/home/pche3675/summer2018/electronmlpid/CentralElectrons_MCdata/data/MC_BkgElectrons_2000000ev.root')
 
 	# Retrieve the ntuple
-	rtree = rfile.Get('data')
-	rentries = rtree.GetEntries()
+	sig_tree = sig_file.Get('data')
+	sig_nentries = sig_tree.GetEntries()
+	bkg_tree = bkg_file.Get('data')	
+	bkg_nentries = bkg_tree.GetEntries() 
 
-	h_LHValue = TH1F("p_LHValue", "LHValue", 101, -1.6, 2)
+	h_sigLH = TH1F("Sig_LHValue", "Signal LHValue", 101, -1.6, 2)
+	h_bkgLH = TH1F("Bkg_LHValue", "Background LHValue", 101, -4, 1.5)
 	
-	# Fill the histogram
-	for i in xrange(rentries):
-		if (i % (rentries/10)) == 0:
-		    print ":: processing entry [%s]... " % i
-
+	# Fill the signal histogram
+	for i in range(sig_nentries):
+	
+		if (i % (sig_nentries/10)) == 0:
+			print ":: processing signal entry [%s]... " % i
+		
 		# Load tree
-		if rtree.LoadTree(i) < 0: 
-		    print "** could not load tree for entry #%s" % i
-		    break
+		if sig_tree.LoadTree(i) < 0:
+			print "** could not load tree for signal entry #%s" % i
+			break
 
-		nb = rtree.GetEntry(i)
+		nb = sig_tree.GetEntry(i)
 		if nb <= 0:
-		    # no data
-		    continue 
-
-	    # Define branches of tree that will be used for cuts
-		p_LHValue = rtree.p_LHValue
-		h_LHValue.Fill(p_LHValue) 
-
-	# Draw the histogram
+			continue
+	
+		p_LHValue = sig_tree.p_LHValue
+		h_sigLH.Fill(p_LHValue)
+		
 	c1 = TCanvas()
-	h_LHValue.Draw()
-	h_LHValue.GetXaxis().SetTitle('Likelihood Value')
-	h_LHValue.GetYaxis().SetTitle('Number of Entries')
-	c1.SaveAs('LHValue_sig.pdf')
+	h_sigLH.Draw()
+	h_sigLH.GetXaxis().SetTitle('Likelihood Value')
+	h_sigLH.GetYaxis().SetTitle('Number of Entries')
+	c1.SaveAs('sig_LH.pdf')
+
+	# Fill the background histogram
+	for i in range(bkg_nentries):
+		
+		if (i % (bkg_nentries/10)) == 0:
+			print ":: processing background entry [%s]... " % i
+		
+		# Load tree
+		if bkg_tree.LoadTree(i) < 0:
+			print "** could not load tree for background entry #%s" % i
+			break
+
+		nb = bkg_tree.GetEntry(i)
+		if nb <= 0:
+			continue
+	
+		p_LHValue = bkg_tree.p_LHValue
+		h_bkgLH.Fill(p_LHValue)
 
 	c2 = TCanvas()
-	h_ROC = TH1F("ROC for Likelihood Fit", "ROC for Likelihood Fit", 101, 1, 1)
-	columns = h_LHValue.GetNbinsX()
+	h_bkgLH.Draw()
+	h_bkgLH.GetXaxis().SetTitle('Likelihood Value')
+	h_bkgLH.GetYaxis().SetTitle('Number of Entries')
+	c2.SaveAs('bkg_LH.pdf')
 
-	total = 0
-	
+	# Calculate TPR and FPR
+	TPR = array('f')
+	FPR = array('f')
+
+	scounter = 0
+	bcounter = 0
+	columns = h_sigLH.GetNbinsX()    #  Both signal and background will have same number
+									 #  of columns
+
+
 	for i in range(columns + 1):
-		total += h_LHValue.GetBinContent(i)
-		ratio = (rentries-total)/rentries
-		h_ROC.Fill(ratio)
-
-	h_ROC.Draw("L")
-	h_ROC.GetXaxis().SetTitle('Signal Efficiency')
-	h_ROC.GetYaxis().SetTitle('Background Rejection')
-	c2.SaveAs('ROC_LHValue.pdf')
+		scounter += h_sigLH.GetBinContent(i)
+		TPR.append((sig_nentries-scounter)/sig_nentries)
+		bcounter += h_bkgLH.GetBinContent(i)
+		FPR.append(1-(bkg_nentries-bcounter)/bkg_nentries)
+	
+	c3 = TCanvas()	
+	gr_ROC = TGraph(len(TPR), TPR, FPR)
+	gr_ROC.Draw("ACP")
+	c3.SaveAs('roc.pdf')
+	
 
 # Call main()
 if __name__ == "__main__":
