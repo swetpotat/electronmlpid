6f36096c0ece85e274e2b74d6839af581adf5ca4
Kevin Varvell <kevin.varvell@sydney.edu.au>
2018-05-08 15:21:22 +1000

    udpated

--- CentralElectrons_MCdata/event_weight.py
+++ CentralElectrons_MCdata/event_weight.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
-from ROOT import TFile, TH1F, TCanvas, TTree
-from ROOT import gDirectory, std
+from ROOT import *
 import sys, getopt
 import math
-from ROOT import *
-from array import *
+import array
 import numpy
 
 gStyle.SetOptStat(1)
@@ -27,13 +25,10 @@ def main():
 	# Make histograms
 	hs_eta = TH1F("Signal Eta", "Eta angle", 101, -2.5, 2.5)
 	hb_eta = TH1F("Background Eta", "Eta angle", 101, -2.5, 2.5)
-	eta_weights = TH1F("eta weights", "eta_weights", 101, -2.5, 2.5)
 	hs_et = TH1F("Signal E_t", "Transverse Energy", 101, 0, 100000)
 	hb_et = TH1F("Background E_t", "Transverse Energy", 101, 0, 100000)
-	et_weights = TH1F("et weights", "et_weights", 101, 0,100000)
 	hs_mu = TH1F("Signal <mu>", "Average Interactions Per Crossing", 41, -0.5, 40.5)
 	hb_mu = TH1F("Background <mu>", "Average Interactions Per Crossing", 41, -0.5, 40.5)
-	mu_weights =TH1F("mu weights", "mu_weights", 41, -0.5, 40.5)
 
 	sn_eta = []
 	sn_mu = []
@@ -71,24 +66,15 @@ def main():
 		bn_mu.append(hb_mu.GetXaxis().FindBin(p_mu))
 		bn_et.append(hb_et.GetXaxis().FindBin(p_et_calo))		
 
-	print("Histograms have been filled.")
-
 	# Calculate weights
-	c1 = TCanvas()
 	eta_weights = hs_eta.Clone()
 	eta_weights.Divide(hb_eta)
-	eta_weights.Draw()
-	c1.SaveAs("eta_weights.pdf")
-	c2 = TCanvas()
 	et_weights = hs_et.Clone()
 	et_weights.Divide(hb_et)
-	et_weights.Draw()
-	c2.SaveAs("et_weights.pdf")
-	c3 = TCanvas()
 	mu_weights = hs_mu.Clone()
 	mu_weights.Divide(hb_mu)
-	mu_weights.Draw()
-	c3.SaveAs("mu_weights.pdf")
+
+	# Assign weights to each event
 	
 	bkg_weights = numpy.array([0])
 	sig_weights = numpy.array([0])	
@@ -101,17 +87,17 @@ def main():
 
 	for i in range(len(sn_eta)):
 		sig_weights = numpy.append(sig_weights,mu_weights.GetBinContent(int(sn_mu[i]))*et_weights.GetBinContent(int(sn_et[i]))*eta_weights.GetBinContent(int(sn_eta[i])))
-		if (i % 100000 == 0):
-			print(str(i) + " signal event weights calculated.....")
+		if (i % 100000 == 0 and i != 0):
+			print(str(i) + " signal event weights assigned.....")
 		ttree.Fill()
 
 	for i in range(len(bn_eta)):
 		bkg_weights = numpy.append(bkg_weights,mu_weights.GetBinContent(int(bn_mu[i]))*et_weights.GetBinContent(int(bn_et[i]))*eta_weights.GetBinContent(int(bn_eta[i])))
-		if (i % 100000 == 0):
-			print(str(i) + " background event weights calculated.....")
+		if (i % 100000 == 0 and i != 0):
+			print(str(i) + " background event weights assigned.....")
 		ttree.Fill()
 
-	print("Weights assigned to each event")
+
 	print("Signal: " + str(len(sn_eta)))
 	print("Background: " + str(len(bn_eta)))
 	print("Total number of events: " + str(bnentries + snentries))
---
+++ CentralElectrons_MCdata/event_weight.sh
@@ -0,0 +1,1 @@
+python3 event_weight.py
